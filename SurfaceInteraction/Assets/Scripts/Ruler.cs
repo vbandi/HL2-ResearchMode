112f83c751330d80ff2c54d4dab0a0f9239c6f98
Velvárt András <andras.velvart@aerinx.com>
2021-03-23 00:44:20 +0100

    Added ruler!

---
+++ SurfaceInteraction/Assets/Scripts/Ruler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Ruler : MonoBehaviour
+{
+    public Vector3 From;
+    public Vector3 To;
+
+    private LineRenderer _line;
+    public TextMeshPro Label;
+
+    private void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        var distance = Vector3.Distance(From, To);
+
+        if (distance < 0.01)
+        {
+            _line.enabled = false;
+            Label.enabled = false;
+            return;
+        }
+
+        _line.enabled = true;
+        Label.enabled = true;
+        
+        _line.SetPositions(new []{From, To});
+
+        Label.transform.position = From + (Vector3.up * 0.05f);
+        Label.text = (distance * 100).ToString("F1");
+    }
+}
--- SurfaceInteraction/Assets/Scripts/SurfaceTouchObserver.cs
+++ SurfaceInteraction/Assets/Scripts/SurfaceTouchObserver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Rendering;
@@ -21,11 +22,15 @@ public class SurfaceTouchObserver : MonoBehaviour
     public GameObject Mouse;
     public GameObject LeftButton;
     public GameObject Pointer;
+
+    public Ruler Ruler;
     
     private Plane _meshPlane;
     public float MouseDownPalmDistanceThreshold = 0.01f;
 
-    private List<Vector3> points = new List<Vector3>(); 
+    private List<Vector3> points = new List<Vector3>();
+
+    private VirtualMouseStatus _mouseStatus = VirtualMouseStatus.None; 
 
     // Start is called before the first frame update
     private void Start()
@@ -36,18 +41,21 @@ public class SurfaceTouchObserver : MonoBehaviour
         _spatialAwarenessLayerId = LayerMask.GetMask("Spatial Awareness");
 
         _line = GetComponent<LineRenderer>();
+
+        this.ObserveEveryValueChanged(x => x._mouseStatus).Subscribe(s => Debug.Log($"Mouse Status: {s}"));
     }
 
     private void Update()
     {
-        //HandPoseUtils.finger curl doesn't work well with vertical hands
-        //Debug.Log($"Index finger curl: {HandPoseUtils.IndexFingerCurl(Handedness.Right)}");
 
+        // Calculate distances. Assume palm normal is more or less perpendicular to surface.
+        
         var rightHand = HandJointUtils.FindHand(Handedness.Right);
 
         if (rightHand == null || rightHand.TrackingState != TrackingState.Tracked)
         {
             Mouse.SetActive(false);
+            _mouseStatus = VirtualMouseStatus.None;
             return;
         }
 
@@ -60,60 +68,89 @@ public class SurfaceTouchObserver : MonoBehaviour
             return;
 
         Plane palmPlane = new Plane(palmPose.Up, palmPose.Position);
-        
-        // Calculate distances. Assume palm normal is more or less perpendicular to surface.
-        
+
         // Step 1: cast ray from palm DOWN, and find intersection with spatial mesh
         var hasHit = Physics.Raycast(palmPose.Position, -palmPose.Up,
             out var raycastResult, PalmDistanceFromSurfaceForMouseToAppear, _spatialAwarenessLayerId);
-        
-        Mouse.SetActive(hasHit);
-        
-        if (!hasHit)
-            return;
-        
-        // Step 2: check if mouse is grabbed and button is "pressed"
 
+        var isGrabbed = hasHit && raycastResult.distance < PalmDistanceFromSurfaceForGrab;
+        
         var thumbDistance = palmPlane.GetDistanceToPoint(thumbTipPose.Position);
         var indexDistance = palmPlane.GetDistanceToPoint(indexTipPose.Position);
         var middleDistance = palmPlane.GetDistanceToPoint(middleTipPose.Position);
         var ringDistance = palmPlane.GetDistanceToPoint(ringTipPose.Position);
         var pinkyDistance = palmPlane.GetDistanceToPoint(pinkyTipPose.Position);
 
-        var isGrabbed = raycastResult.distance < PalmDistanceFromSurfaceForGrab;
-        
-        Pointer.SetActive(isGrabbed);
-        
-        if (!isGrabbed)
-            _meshPlane = new Plane(raycastResult.normal, raycastResult.point);
-        
-        bool isMouseDown = isGrabbed && indexDistance < -MouseDownPalmDistanceThreshold;
+        bool isMouseDown = indexDistance < -MouseDownPalmDistanceThreshold;
 
-        LeftButton.EnsureComponent<MaterialInstance>().Material.color = isMouseDown ? Color.white : Color.gray;
-            
-        // Step 3: identify spatial mesh plane at the above intersection
-        if (isMouseDown)
+        switch (_mouseStatus)
         {
-            points.Add(Pointer.transform.position);
-        }
-        else
-        {
-            points.Clear();
-        }
+            case VirtualMouseStatus.None:
+                if (hasHit)
+                    _mouseStatus = VirtualMouseStatus.Visible;
+                
+                Mouse.SetActive(hasHit);
+                break;
+            case VirtualMouseStatus.Visible:
+                if (!hasHit)
+                    _mouseStatus = VirtualMouseStatus.None;
+                else
+                {
+                    _meshPlane = new Plane(raycastResult.normal, raycastResult.point);
+                    SetMousePosition(palmPose, true);
 
+                    if (isGrabbed)
+                        _mouseStatus = VirtualMouseStatus.Grabbed;
+                }
+                break;
+            case VirtualMouseStatus.Grabbed:
+                SetMousePosition(palmPose, false);
+                Pointer.SetActive(isGrabbed);
+                if (!isGrabbed)
+                    _mouseStatus = VirtualMouseStatus.Visible;
+
+                if (isGrabbed && isMouseDown)
+                    _mouseStatus = VirtualMouseStatus.ButtonPressed; 
+                break;
+            case VirtualMouseStatus.ButtonPressed:
+                SetMousePosition(palmPose, false);
+                
+                LeftButton.EnsureComponent<MaterialInstance>().Material.color = isMouseDown ? Color.white : Color.gray;
+                
+                // Step 3: identify spatial mesh plane at the above intersection
+                if (isMouseDown)
+                {
+                    points.Add(Pointer.transform.position);
+                    _line.positionCount = points.Count;
+                    _line.SetPositions(points.ToArray());
+
+                    if (points.Any())
+                    {
+                        Ruler.From = points.First();
+                        Ruler.To = points.Last();
+                    }
+                }
+                else
+                {
+                    points.Clear();
+                    _mouseStatus = VirtualMouseStatus.Grabbed;
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void SetMousePosition(MixedRealityPose palmPose, bool setRotation)
+    {
         Mouse.transform.position = _meshPlane.ClosestPointOnPlane(palmPose.Position);
         
+        
         // TODO: doesn't work on vertical surfaces
-        Mouse.transform.LookAt(Mouse.transform.position + Vector3.ProjectOnPlane(palmPose.Forward, _meshPlane.normal));  
-        
-        // Step 3: get distance between index finger and spatial mesh plane above
-        _line.positionCount = points.Count;
-        _line.SetPositions(points.ToArray());
-        
-        // Debug.Log($"Palm-Surface: {PalmDistanceFromSurface}. IsGrabbed: {isGrabbed}, IsMouseDown: {isMouseDown}");
+        if (setRotation)
+            Mouse.transform.LookAt(Mouse.transform.position + Vector3.ProjectOnPlane(palmPose.Forward, _meshPlane.normal));
     }
-    
-    
+
 
     private Subject<MixedRealityPose?> JointToObservable(Handedness handedness, TrackedHandJoint joint)
     {
@@ -136,4 +173,12 @@ public class SurfaceTouchObserver : MonoBehaviour
         result.DoOnTerminate(subscription.Dispose);
         return result;
     }
+}
+
+public enum VirtualMouseStatus
+{
+    None,
+    Visible,
+    Grabbed,
+    ButtonPressed
 }
